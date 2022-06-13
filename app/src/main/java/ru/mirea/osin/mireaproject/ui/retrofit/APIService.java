fa4675eeaaecaf5ac68d0cb1699c7e5c62ac1163
Alex <alex.osin1602@gmail.com>
2022-06-13 21:36:38 +0300

    Done

---
+++ app/src/main/java/ru/mirea/osin/mireaproject/ui/retrofit/APIService.java
@@ -0,0 +1,10 @@
+package ru.mirea.osin.mireaproject.ui.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIService {
+    @GET("heroes.php")
+    Call<Heroes> getHeroes();
+}
+
