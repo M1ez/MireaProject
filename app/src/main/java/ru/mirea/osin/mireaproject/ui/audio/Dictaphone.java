4b5e0f13522b51b98330972ad849ab2ad6cc00f9
Alex <alex.osin1602@gmail.com>
2022-06-14 20:18:13 +0300

    fix dictophone

---
+++ app/src/main/java/ru/mirea/osin/mireaproject/ui/audio/Dictaphone.java
@@ -0,0 +1,346 @@
+package ru.mirea.osin.mireaproject.ui.audio;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.transition.TransitionManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import ru.mirea.osin.mireaproject.R;
+
+public class Dictaphone extends Fragment {
+    private MediaRecorder mRecorder;
+    private MediaPlayer mPlayer;
+    private String fileName = null;
+    private int lastProgress = 0;
+    private Handler mHandler = new Handler();
+    private boolean isPlaying = false;
+    private Toolbar toolbar;
+    private Chronometer chronometer;
+    private ImageView imageViewRecord, imageViewPlay, imageViewStop;
+    private SeekBar seekBar;
+    private LinearLayout linearLayoutRecorder, linearLayoutPlay;
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+    private static final int REQUEST_CODE_PERMISSION = 100;
+    private boolean isWork=false;
+    private String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            RECORD_AUDIO
+    };
+    private File file;
+    public static Dictaphone newInstance(String param1, String param2) {
+        Dictaphone fragment = new Dictaphone();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_audio, null);
+
+
+        /** setting up the toolbar  **/
+        toolbar = (Toolbar) v.findViewById(R.id.toolbar);
+        toolbar.setTitle("Voice Recorder");
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.black));
+        // setSupportActionBar(toolbar);
+        int dictaphonePermissionStatus =
+                ContextCompat.checkSelfPermission(getActivity(), RECORD_AUDIO);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (dictaphonePermissionStatus == PackageManager.PERMISSION_GRANTED && storagePermissionStatus == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        } else {
+            // Выполняется запрос к пользователь на получение необходимых разрешений
+            ActivityCompat.requestPermissions(getActivity(), new String[] {RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE_PERMISSION);
+        }
+        linearLayoutRecorder = (LinearLayout) v.findViewById(R.id.linearLayoutRecorder);
+        chronometer = (Chronometer) v.findViewById(R.id.chronometerTimer);
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        imageViewRecord = (ImageView) v.findViewById(R.id.imageViewRecord);
+        imageViewStop = (ImageView) v.findViewById(R.id.imageViewStop);
+        imageViewPlay = (ImageView) v.findViewById(R.id.imageViewPlay);
+        linearLayoutPlay = (LinearLayout) v.findViewById(R.id.linearLayoutPlay);
+        seekBar = (SeekBar) v.findViewById(R.id.seekBar);
+        isWork = hasPermissions(getActivity(), PERMISSIONS);
+        if (!isWork) {
+            ActivityCompat.requestPermissions(getActivity(), PERMISSIONS,
+                    REQUEST_CODE_PERMISSION);
+        }
+        imageViewRecord.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) { if( v == imageViewRecord ){
+                try {
+                    prepareforRecording();
+                    startRecording();
+                }
+                catch (Exception e) { Toast.makeText(getActivity(), "Eror",
+                        Toast.LENGTH_SHORT).show();}
+
+            }}  });
+        imageViewStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {if( v == imageViewStop ){
+                prepareforStop();
+                stopRecording();
+                processAudioFile();
+            }}  });
+        imageViewPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {if( v == imageViewPlay ){
+                if( !isPlaying && fileName != null ){
+                    isPlaying = true;
+                    // getActivity().startService(new Intent(getActivity(),DictaphoneService.class));
+                    startPlaying();
+                }else{
+                    isPlaying = false;
+                    // getActivity().stopService(new Intent(getActivity(),DictaphoneService.class));
+                    stopPlaying();
+                }}  }});
+
+        return v;
+    }
+
+
+
+
+    // Callback with the request from calling requestPermissions(...)
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            // permission granted
+            isWork = grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void processAudioFile() {
+        ContentValues values = new ContentValues(4);
+        long current = System.currentTimeMillis();
+        // установка meta данных созданному файлу
+        values.put(MediaStore.Audio.Media.TITLE, "audio" + file.getName());
+        values.put(MediaStore.Audio.Media.DATE_ADDED, (int) (current / 1000));
+        values.put(MediaStore.Audio.Media.MIME_TYPE, "audio/3gpp");
+        values.put(MediaStore.Audio.Media.DATA, file.getAbsolutePath());
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        Uri baseUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Uri newUri = contentResolver.insert(baseUri, values);
+        // оповещение системы о новом файле
+        getActivity().sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, newUri));
+        Toast.makeText(getActivity(), "This is a progress!",
+                Toast.LENGTH_SHORT).show();
+    }
+    private void prepareforRecording() {
+        TransitionManager.beginDelayedTransition(linearLayoutRecorder);
+        imageViewRecord.setVisibility(View.GONE);
+        imageViewStop.setVisibility(View.VISIBLE);
+        linearLayoutPlay.setVisibility(View.GONE);
+    }
+    private void startPlaying() {
+
+        mPlayer = new MediaPlayer();
+        try {
+//fileName is global string. it contains the Uri to the recently recorded audio.
+            mPlayer.setDataSource(fileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e("LOG_TAG", "prepare() failed");
+        }
+        //making the imageview pause button
+        imageViewPlay.setImageResource(R.drawable.ic_action_pause);
+
+        seekBar.setProgress(lastProgress);
+        mPlayer.seekTo(lastProgress);
+        seekBar.setMax(mPlayer.getDuration());
+        seekUpdation();
+        chronometer.start();
+
+        /** once the audio is complete, timer is stopped here**/
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                imageViewPlay.setImageResource(R.drawable.ic_action_play);
+                isPlaying = false;
+                chronometer.stop();
+            }
+        });
+
+        /** moving the track as per the seekBar's position**/
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if( mPlayer!=null && fromUser ){
+                    //here the track's progress is being changed as per the progress bar
+                    mPlayer.seekTo(progress);
+                    //timer is being updated as per the progress of the seekbar
+                    chronometer.setBase(SystemClock.elapsedRealtime() - mPlayer.getCurrentPosition());
+                    lastProgress = progress;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            seekUpdation();
+        }
+    };
+
+    private void seekUpdation() {
+        if(mPlayer != null){
+            int mCurrentPosition = mPlayer.getCurrentPosition() ;
+            seekBar.setProgress(mCurrentPosition);
+            lastProgress = mCurrentPosition;
+        }
+        mHandler.postDelayed(runnable, 100);
+    }
+
+    private void startRecording() {
+        //we use the MediaRecorder class to record
+
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        /**In the lines below, we create a directory VoiceRecorderSimplifiedCoding/Audios in the phone storage
+         * and the audios are being stored in the Audios folder **/
+        File root = getActivity().getExternalFilesDir(
+                Environment.DIRECTORY_MUSIC);
+        file = new File(root.getAbsolutePath() + "/VoiceRecorderSimplifiedCoding/Audios");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        fileName = root.getAbsolutePath() + "/VoiceRecorderSimplifiedCoding/Audios/" +
+                System.currentTimeMillis() + ".mp3";
+        Log.d("filename",fileName);
+        mRecorder.setOutputFile(fileName);
+
+
+        try {
+            mRecorder.prepare();
+            mRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        lastProgress = 0;
+        seekBar.setProgress(0);
+        stopPlaying();
+        //starting the chronometer
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.start();
+    }
+    private void prepareforStop() {
+        TransitionManager.beginDelayedTransition(linearLayoutRecorder);
+        imageViewRecord.setVisibility(View.VISIBLE);
+        imageViewStop.setVisibility(View.GONE);
+        linearLayoutPlay.setVisibility(View.VISIBLE);
+    }
+
+    private void stopRecording() {
+
+        try{
+            mRecorder.stop();
+            mRecorder.release();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mRecorder = null;
+        //starting the chronometer
+        chronometer.stop();
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        //showing the play button
+        Toast.makeText(getActivity(), "Recording saved successfully.", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    private void stopPlaying() {
+
+        try{
+            mPlayer.release();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mPlayer = null;
+        //showing the play button
+        imageViewPlay.setImageResource(R.drawable.ic_action_play);
+        chronometer.stop();
+    }
+}
